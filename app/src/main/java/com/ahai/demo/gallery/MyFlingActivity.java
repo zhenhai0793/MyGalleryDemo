279f748c92a5009acb8ca3ab3f1bc0e7d2482316
fengzhenhai <zhenhai.fzh@alibaba-inc.com>
2017-03-10 17:01:19 +0800

    添加MyFlingActivity;实现可以上下滑动且透明度发生渐变的ImageView

---
+++ app/src/main/java/com/ahai/demo/gallery/MyFlingActivity.java
@@ -0,0 +1,78 @@
+package com.ahai.demo.gallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import nsouth.jonas.android.R;
+
+public class MyFlingActivity extends Activity {
+
+    String TAG = "MyFlingActivity";
+
+    GestureDetector mGestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_fling);
+        initGestureDetector();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    private void initGestureDetector() {
+        mGestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                float minMove = 60;         //最小滑动距离
+                float minVelocity = 0;      //最小滑动速度
+                float beginX = e1.getX();
+                float endX = e2.getX();
+                float beginY = e1.getY();
+                float endY = e2.getY();
+
+                if (beginX - endX > minMove && Math.abs(velocityX) > minVelocity) {   //左滑
+                    Log.d(TAG, "左滑速度:" + velocityX);
+                } else if (endX - beginX > minMove && Math.abs(velocityX) > minVelocity) {   //右滑
+                    Log.d(TAG, "右滑速度:" + velocityX);
+                } else if (beginY - endY > minMove && Math.abs(velocityY) > minVelocity) {   //上滑
+                    Log.d(TAG, "上滑速度:" + velocityY);
+                } else if (endY - beginY > minMove && Math.abs(velocityY) > minVelocity) {   //下滑
+                    Log.d(TAG, "下滑速度:" + velocityY);
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ahai/demo/gallery/MyImageView.java
@@ -0,0 +1,91 @@
+package com.ahai.demo.gallery;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+/**
+ * Created by zhenhai.fzh on 17/3/10.
+ */
+
+public class MyImageView extends ImageView {
+
+    private String TAG = "MyImageView";
+
+    private int lastX;
+    private int lastY;
+    private int firstY;
+    private float firstRawX;
+    private float firstRawY;
+
+    public MyImageView(Context context) {
+        super(context);
+    }
+
+    public MyImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //获取到手指处的横坐标和纵坐标
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        float rawX = event.getRawX();
+        float rawY = event.getRawY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                lastX = x;
+                lastY = y;
+                firstY = y;
+                firstRawX = rawX;
+                firstRawY = rawY;
+
+                break;
+
+            case MotionEvent.ACTION_MOVE: {
+                //计算移动的距离
+//                int offX = x - lastX;
+                int offX = 0;
+                int offY = y - lastY;
+                //调用layout方法来重新放置它的位置
+                layout(getLeft() + offX, getTop() + offY,
+                        getRight() + offX, getBottom() + offY);
+
+                updateMyAlpha(rawY);
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                int offX = 0;
+                int offY = (int)(firstRawY - rawY);
+                //调用layout方法来重新放置它的位置
+                layout(getLeft() + offX, getTop() + offY,
+                        getRight() + offX, getBottom() + offY);
+                updateMyAlpha(firstRawY);
+                break;
+            }
+        }
+        return true;
+//        return super.onTouchEvent(event);
+    }
+
+    private void updateMyAlpha(float curRawY) {
+        if(curRawY > firstRawY) return;
+        float height = getHeight();
+        float moveHeight = Math.abs(curRawY - firstRawY);
+        int factor = 1;
+        float alpha = 1-moveHeight/height*factor;
+        Log.d(TAG, "height:"+height+", firstRawY:"+firstRawY+", curRawY:"+curRawY+", moveHeight:"+moveHeight+", alpha:"+alpha);
+        setAlpha(alpha);
+    }
+}
